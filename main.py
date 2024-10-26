9471e6fd92019fbe011344fd7cfff9e7b00ae64f
Maximilian Amthor <amthormaximilian@gmail.com>
2024-10-26 11:04:09 +0200

    apply modifiers (shift, altgr) to reasonable extent, small updates/fixes in mappings

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import time
 import evdev
 from dotenv import load_dotenv
 from evdev import InputEvent
+from mappings import altgr_conversion, shift_conversion, keycode_conversion
 
 # Define format of input_event structure (24 bytes)
 # 'Q' for tv_sec (8 bytes), 'Q' for tv_usec (8 bytes), 'H' for type (2 bytes),
@@ -73,6 +74,72 @@ def filter_key_down_evts(evts: list[InputEvent]) -> list[evdev.InputEvent]:
     return key_down_evts
 
 
+class PressedKeyEvt:
+    def __init__(self, value: str, timestamp: float):
+        self.value = value
+        self.timestamp = timestamp
+
+
+class KeyModifier:
+    def __init__(self, timestamp: float, active: bool):
+        self.timestamp = timestamp
+        self.active = active
+
+
+def deactivate_modifiers_if_expired(modifiers: list[KeyModifier], max_allowed_age: float) -> None:
+    """
+    Deactivate modifiers in list if they are too old (we have probably missed a keyup event if we are registering a 'Shift'-Keypress for 10 seconds straight
+    :param modifiers: Modifiers to (potentially) deactivate
+    :return: None
+    """
+
+    now = time.time()
+    for modifier in modifiers:
+        if modifier.timestamp - now > max_allowed_age:
+            modifier.active = False
+
+
+def update_modifiers(evt: InputEvent, altgr: KeyModifier, shift: KeyModifier, state: bool) -> [int, int]:
+    categorized = evdev.categorize(evt)
+    # Update modifiers
+    if categorized.keycode == 'KEY_RIGHTALT':
+        altgr.active = state
+        altgr.timestamp = evt.timestamp()
+    elif categorized.keycode in ['KEY_LEFTSHIFT', 'KEY_RIGHTSHIFT']:
+        shift.active = state
+        shift.timestamp = evt.timestamp()
+
+    deactivate_modifiers_if_expired([altgr, shift], 10)  # Deactivate after 10 seconds
+
+
+def filter_key_down_evts_apply_modifiers(evts: list[InputEvent]) -> list[PressedKeyEvt]:
+    """
+    Filter the events for keydown presses, returning a list of form ['a', 'F', '{', 'a', 'Z', ...],
+    i.e. with modifiers (shift, altgr) applied
+    :param evts:
+    :return:
+    """
+    shift = KeyModifier(0, False)
+    altgr = KeyModifier(0, False)
+    key_down_evts = []
+    for evt in evts:
+        categorized = evdev.categorize(evt)
+        if isinstance(categorized, evdev.KeyEvent):  # Key
+            if categorized.keystate == evdev.KeyEvent.key_down:  # Keydown
+                update_modifiers(evt, altgr, shift, True)
+                key_value = keycode_conversion.get(evt.code, evdev.ecodes.keys[evt.code])
+                # Altgr takes precedence over shift (as per our convention)
+                if altgr.active:
+                    key_value = altgr_conversion.get(key_value, key_value)
+                elif shift.active:
+                    key_value = shift_conversion.get(key_value, key_value)
+                key_down_evts.append(PressedKeyEvt(key_value, evt.timestamp()))
+            elif categorized.keystate == evdev.KeyEvent.key_up:  # Keyup
+                update_modifiers(evt, altgr, shift, False)
+
+    return key_down_evts
+
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -83,7 +150,7 @@ logging.basicConfig(
 )
 
 
-def evts_to_frequencies(evts: list[InputEvent], multivalue_code_mappings: dict[str, list[str]]) -> dict[str, int]:
+def evts_to_frequencies(evts: list[PressedKeyEvt], multivalue_code_mappings: dict[str, list[str]]) -> dict[str, int]:
     """
     :param evts:
     :param multivalue_code_mappings: Maps code name to list of values it maps (would be better the other way around,
@@ -92,7 +159,7 @@ def evts_to_frequencies(evts: list[InputEvent], multivalue_code_mappings: dict[s
     """
     frequencies = dict()
     for evt in evts:
-        code_name = evdev.ecodes.keys[evt.code]
+        code_name = evt.value
         if isinstance(code_name, list):
             code_names = code_name
             # Find matching list and its specified code (if there are multiple matches, this depends on items())
